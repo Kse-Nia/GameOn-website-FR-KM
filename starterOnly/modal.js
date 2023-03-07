c75527c6496dd77640b138e13c6c36553afc0e3c
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-03-07 15:51:35 +0100

    close modal button - working

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,19 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const form = document.getElementsByName("reserve"); // Get form
+const closeBtn = document.querySelectorAll(".close"); // Get close modal button
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn[0].addEventListener("click", closeModal); // Close modal event on click
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
